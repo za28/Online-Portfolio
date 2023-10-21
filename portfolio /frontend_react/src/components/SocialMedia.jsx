5e709746378e367167a96276029b4b4859e5815c
Zahra Amaan <114661472+za28@users.noreply.github.com>
2023-10-21 22:40:48 +0100

    add frontend and backend

---
+++ portfolio /frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BsGithub} from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+    <a href="https://github.com/za28" target="_blank" rel="noopener noreferrer">
+        <BsGithub >
+    
+        </BsGithub>
+      </a>
+    </div>
+    
+    
+  </div>
+);
+
+export default SocialMedia;
---
+++ portfolio /frontend_react/src/constants/images.js
@@ -0,0 +1,91 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import api from '../assets/api.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import flutter from '../assets/flutter.png';
+import git from '../assets/git.png';
+import github from '../assets/github.png';
+import graphql from '../assets/graphql.png';
+import html from '../assets/html.png';
+import java from '../assets/java.png';
+import javascript from '../assets/javascript.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+import vue from '../assets/vue.png';
+import scala from '../assets/scala.png';
+import csharp from '../assets/csharp.png';
+import sql from '../assets/sql.png';
+
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+import circle from '../assets/circle.svg';
+import logo from '../assets/logo.png';
+
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+import asus from '../assets/asus.png';
+import bolt from '../assets/bolt.png';
+import nb from '../assets/nb.png';
+import skype from '../assets/skype.png';
+import spotify from '../assets/spotify.png';
+
+import ZAHRA from '../assets/ZAHRA.png'
+
+
+import heart from '../assets/heart_memoji.png'
+import wave from '../assets/wave_memoji.png'
+import wave2 from '../assets/wave_memoji2.png'
+
+export default {
+  email,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  git,
+  github,
+  graphql,
+  html,
+  java,
+  javascript,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  vue,
+  scala,
+  csharp,
+  sql,
+  about01,
+  about02,
+  about03,
+  about04,
+  circle,
+  logo,
+  adidas,
+  amazon,
+  asus,
+  bolt,
+  nb,
+  skype,
+  spotify,
+  ZAHRA,
+  heart,
+  wave,
+  wave2,
+};
